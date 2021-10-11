843db9138d0cdab2ac199da3d23796ca8c947d73
Githb-usr <tragopan@free.fr>
2021-10-11 11:43:11 +0200

    feat display faq : faq list & faq detail

--- contents/urls.py
+++ contents/urls.py
@@ -6,6 +6,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(route='news/', view=views.NewsListView.as_view(), name='article-list'),
-    path(route='news/<int:pk>/', view=views.NewsDetailView.as_view(), name='article-detail'),
+    path(route='news/', view=views.NewsListView.as_view(), name='news-list'),
+    path(route='news/<int:pk>/', view=views.NewsDetailView.as_view(), name='news-detail'),
+    path(route='faq/', view=views.FaqListView.as_view(), name='faq-list'),
+    path(route='faq/<int:pk>/', view=views.FaqDetailView.as_view(), name='faq-detail'),
 ]
