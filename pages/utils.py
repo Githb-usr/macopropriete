bc890bcc8ab11810f1835011904bd456e9269cba
Githb-usr <tragopan@free.fr>
2021-11-01 10:26:56 +0100

    refactor(utils): genereic_send_mail in pages + choice_translation

---
+++ pages/utils.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+
+from config.settings import EMAIL_HOST_USER, ADMIN_RECIPIENTS
+
+def choice_translation(choice_list, original_item):
+    translated_item = ''
+    for item in choice_list:
+        if item[0] == original_item:
+            translated_item = item[1]
+
+    return translated_item
+
+def generic_send_mail(mail_subject, html_page, recipients, email_context):
+    subject = mail_subject
+    from_email = EMAIL_HOST_USER
+    html_message = render_to_string(html_page, email_context)
+    plain_message = strip_tags(html_message)
+    send_mail(subject, plain_message, from_email, [recipients], html_message=html_message)
