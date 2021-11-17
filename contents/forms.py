e9250eddb6b58838ddf30154ab389e3e74f740ef
Githb-usr <tragopan@free.fr>
2021-11-17 13:55:14 +0100

    feat(forms): add PhotoForm

--- contents/forms.py
+++ contents/forms.py
@@ -5,13 +5,21 @@ from django import forms
 
 from contents import models
 
+class PhotoForm(forms.ModelForm):
+    """
+    Form used for photo upload
+    """
+    class Meta:
+        model = models.Photo
+        fields = ['image', 'caption']
+
 class NewsForm(forms.ModelForm):
     """
     Form used for the profile update
     """
     class Meta: 
         model = models.News
-        fields = ('category', 'title', 'content', 'image', 'status', )
+        fields = ('category', 'title', 'content', 'status', )
 
 class NewsUpdateForm(forms.ModelForm):
     class Meta: 
