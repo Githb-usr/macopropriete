12f60395ad15a898ae5aa06bc308d3159a62a6ab
Githb-usr <tragopan@free.fr>
2021-11-22 15:47:34 +0100

    feat(forms): add faq and event forms

--- contents/forms.py
+++ contents/forms.py
@@ -1,27 +1,63 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from django import forms
+from django.forms import ModelForm, DateTimeField, SplitDateTimeField, widgets
 
 from contents import models
 
-class PhotoForm(forms.ModelForm):
+class PhotoForm(ModelForm):
     """
     Form used for photo upload
     """
     class Meta:
         model = models.Photo
-        fields = ['image', 'caption']
+        fields = ('image', 'caption')
 
-class NewsForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(PhotoForm, self).__init__(*args, **kwargs)
+        self.fields['image'].required = False
+
+class NewsForm(ModelForm):
     """
-    Form used for the profile update
+    Form used for create a news
     """
     class Meta: 
         model = models.News
         fields = ('category', 'title', 'content', 'status', )
 
-class NewsUpdateForm(forms.ModelForm):
+class NewsUpdateForm(ModelForm):
     class Meta: 
         model = models.NewsUpdate
         fields = ('update_reason',)
+
+class FaqForm(ModelForm):
+    """
+    Form used for create a FAQ question
+    """
+    class Meta: 
+        model = models.Faq
+        fields = ('category', 'question', 'answer', 'status', )
+
+class EventForm(ModelForm):
+    """
+    Form used for create an event
+    """
+    start_date = SplitDateTimeField(
+        label='Début de l\'évènement (Date / Heure)',
+        widget=widgets.SplitDateTimeWidget(
+            date_attrs={'type': 'date', 'style': 'width:25%;'},
+            time_attrs={'type': 'time', 'style': 'width:25%;'},
+            date_format='%d/%m/%Y',
+            time_format="'%H:%M'"),
+    )
+    end_date = SplitDateTimeField(
+        label='Fin de l\'évènement (Date / Heure)',
+        widget=widgets.SplitDateTimeWidget(
+            date_attrs={'type': 'date', 'style': 'width:25%;'},
+            time_attrs={'type': 'time', 'style': 'width:25%;'},
+            date_format='%d/%m/%Y',
+            time_format="'%H:%M'"),
+    )
+    class Meta: 
+        model = models.Event
+        fields = ('category', 'title', 'content', 'start_date', 'end_date', 'status', )
