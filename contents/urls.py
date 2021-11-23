d28ef5a2525907197313465e7a06227006da6742
Githb-usr <tragopan@free.fr>
2021-11-23 18:42:57 +0100

    feat(urls): add event update & delete routes

--- contents/urls.py
+++ contents/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path(route='events/ended', view=views.EventListOldView.as_view(), name='event-list-old'),
     path(route='events/<uuid:uuid>/', view=views.EventDetailView.as_view(), name='event-detail'),
     path(route='events/create/', view=views.event_create_view, name='event-create'),
+    path(route='events/<uuid:uuid>/update', view=views.event_update_view, name='event-update'),
+    path(route='events/<uuid:uuid>/delete', view=views.event_delete_view, name='event-delete'),
 ]
