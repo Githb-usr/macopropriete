8bb6746b4c07d51a3314c1c7ae369e9957894e10
Githb-usr <tragopan@free.fr>
2021-11-28 18:16:39 +0100

    refactor(app): create incidents app

---
+++ incidents/settings.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Inident categories
+####################
+ACCIDENT = 'ACCIDENT'
+BREAKDOWN = 'BREAKDOWN'
+DAMAGE = 'DAMAGE'
+MISCELLANEOUS = 'MISCELLANEOUS'
+WATER_LEAKAGE = 'WATER_LEAKAGE'
+
+INCIDENT_CATEGORY = [
+    (ACCIDENT, 'Accident'),
+    (DAMAGE, 'Dégradation'),
+    (WATER_LEAKAGE, 'Fuite d\'eau'),
+    (BREAKDOWN, 'Panne'),        
+    (MISCELLANEOUS, 'Divers'),
+]
+
+# Incident tracking status
+##########################
+PENDING = 'PENDING'
+REJECTED = 'REJECTED'
+REGISTERED = 'REGISTERED'
+IN_PROGRESS = 'IN_PROGRESS'
+CLOSED = 'CLOSED'
+
+TRACKING_STATUS = [
+    (PENDING, 'en attente'),
+    (REJECTED, 'rejeté'),
+    (REGISTERED, 'enregistré'),
+    (IN_PROGRESS, 'en cours'),
+    (CLOSED, 'fermé'),
+]
